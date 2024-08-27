dd6f4be6c17d83ff8f2c06a5108d33846bdaefc4
Daniel Liden <djliden91@gmail.com>
2024-08-27 09:19:16 -0500

    adds more to Jupyter tutorial

--- tutorial/app_0.py
+++ tutorial/app_0.py
@@ -2,8 +2,11 @@ from fasthtml.common import *
 
 app, rt = fast_app()
 
+
 @app.get("/")
 def hello():
-    return Titled("Hello, World!")
+    return (Title("Hello, World!"), Main(H1("Hello, World!"), cls="container"),
+            Div(P("Goodbye, World!"), cls="container"))
+
 
 serve()
