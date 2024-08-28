52b6f04a49970f56cfffd8adf3da3f832114df70
Daniel Liden <djliden91@gmail.com>
2024-08-28 09:35:00 -0500

    finishes first tutorial

--- tutorial/app_0.py
+++ tutorial/app_0.py
@@ -6,7 +6,13 @@ app, rt = fast_app()
 @app.get("/")
 def hello():
     return (Title("Hello, World!"), Main(H1("Hello, World!"), cls="container"),
-            Div(P("Goodbye, World!"), cls="container"))
+            Div(P("Goodbye, World!"), A("About", href="/about"), cls="container"))
+
+@rt("/about")
+def get():
+    return(Titled("About this Site",
+                  P("This is an example site built with FastHTML!"),
+                  A("Return Home", href="/")))
 
 
 serve()
